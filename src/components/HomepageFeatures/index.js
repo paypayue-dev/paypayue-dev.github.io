f2f6f51d93871f934caae87431f55c8b87bcad27
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-06-20 10:05:13 +0100

    chore: update homepage to use translation

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,69 +2,84 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
-
-const FeatureList = [
-  {
-    title: 'Guias',
-    imgSrc: 'img/guias.png',
-    description: (
-      <>
-        Este guia descreve os requisitos e passos necessários para a integração com a PayPay para os clientes que pretendam integrar os nossos serviços de pagamento diretamente nas suas aplicações.
-      </>
-    ),
-    linkTo: '/intro'
-  },
-  {
-    title: 'Especificação API',
-    imgSrc: 'img/api.png',
-    description: (
-      <>
-        A PayPay suporta ainda integrações com o seu software de gestão através da nossa API
-      </>
-    ),
-    linkTo: '/api'
-  },
-  {
-    title: 'Integrações',
-    imgSrc: 'img/plugin.png',
-    description: (
-      <>
-        A PayPay disponibiliza diversas integrações e plugins gratuitos que permitem unir os serviços de pagamento, por Multibanco, Cartão de Crédito/Débito e MB WAY, com lojas de e-commerce e softwares de faturação.
-      </>
-    ),
-    linkTo: '/plugins'
-  },
-];
-
-function Feature({imgSrc, title, description, linkTo}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img src={imgSrc} className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-      <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to={linkTo}>
-            {`Consultar ${title}`}
-          </Link>
-      </div>
-    </div>
-  );
-}
+import Translate from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function HomepageFeatures() {
+  const {
+    siteConfig: {customFields}
+  } = useDocusaurusContext();
+
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img src='img/guias.png' className={styles.featureSvg} role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <Heading as="h3">
+                <Translate id="homepage.guias">Guias</Translate>
+              </Heading>
+              <p>
+                <Translate id="homepage.guiasDescription">
+                  Este guia descreve os requisitos e passos necessários para a integração com a PayPay para os clientes que pretendam integrar os nossos serviços de pagamento diretamente nas suas aplicações.
+                </Translate>
+              </p>
+            </div>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                to='/docs/guides/intro'>
+                <Translate id="homepage.guiasButton">Consultar Guias</Translate>
+              </Link>
+            </div>
+          </div>
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img src='img/API.png' className={styles.featureSvg} role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <Heading as="h3">
+                <Translate id="homepage.api">Especificação API</Translate>
+              </Heading>
+              <p>
+                <Translate id="homepage.apiDescription">
+                  A PayPay suporta ainda integrações com o seu software de gestão através da nossa API
+                </Translate>
+              </p>
+            </div>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                href={customFields.docsApi}>
+                <Translate id="homepage.apiButton">Consultar Especificação API</Translate>
+              </Link>
+            </div>
+          </div>
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img src='img/plugin.png' className={styles.featureSvg} role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <Heading as="h3">
+                <Translate id="homepage.integracoes">Integrações</Translate>
+              </Heading>
+              <p>
+                <Translate id="homepage.integracoesDescription">
+                  A PayPay disponibiliza diversas integrações e plugins gratuitos que permitem unir os serviços de pagamento, por Multibanco, Cartão de Crédito/Débito e MB WAY, com lojas de e-commerce e softwares de faturação.
+                </Translate>
+              </p>
+            </div>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                to='/docs/integrations/intro'>
+                <Translate id="homepage.integracoesButton">Consultar Integrações</Translate>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
 import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-// import Heading from '@theme/Heading';
+import Translate from '@docusaurus/Translate';
 import Logo from '@site/static/img/logo-full.svg'
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <div style={{width: '200px', margin: '0 auto'}}>
           <Logo />
         </div>
-        <p className="hero__subtitle">A maneira mais simples de gerir pagamentos</p>
+        <p className="hero__subtitle">
+          <Translate id="homepage.headerDescription">
+            A maneira mais simples de gerir pagamentos
+          </Translate>
+        </p>
       </div>
     </header>
   );
@@ -24,7 +25,7 @@ function HomepageHeader() {
 export default function Home() {
   return (
     <Layout>
-    <HomepageHeader />
+      <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
